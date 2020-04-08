8d442dbb1106c79b704173e02036706746891a4c
Nikita Elsakov <vocasle@gmail.com>
2020-04-08 15:02:40 +0300

    Add MiniGame class and Boost locale to dependencies

---
+++ SpaceBattle/include/SpaceBattle/MiniGame.h
@@ -0,0 +1,22 @@
+#ifndef MINI_GAME_H
+#define MINI_GAME_H
+
+#include <SpaceBattle/SpaceShip.h>
+#include <SpaceBattle/Point.h>
+
+#include <vector>
+
+
+class MiniGame
+{
+public:
+	MiniGame(const std::vector<SpaceShip>& ships);
+	bool play();
+private:
+	Point prompt();
+	std::vector<Point> m_points;
+};
+
+std::vector<Point> get_points(const std::vector<SpaceShip>& ships);
+
+#endif // !MINI_GAME_H
---
+++ SpaceBattle/src/MiniGame.cpp
@@ -0,0 +1,34 @@
+#include <SpaceBattle/MiniGame.h>
+
+#include <algorithm>
+
+MiniGame::MiniGame(const std::vector<SpaceShip>& ships): 
+	m_points{get_points(ships)}
+{ }
+
+bool MiniGame::play()
+{
+	// promt user for input
+	Point p = prompt();
+	// check if point is unique
+	return std::find(m_points.begin(), m_points.end(), p) == m_points.end();
+}
+
+Point prompt()
+{
+	std::wcout << "Enter coordinates for the jump (X Y Z): ";
+	Point p{};
+	std::cin >> p;
+	return p;
+}
+
+std::vector<Point> get_points(const std::vector<SpaceShip>& ships)
+{
+	std::vector<Point> points;
+	for (auto& ship : ships)
+	{
+		auto pos = ship.get_position();
+		points.insert(points.end(), pos.begin(), pos.end());
+	}
+	return points;
+}
